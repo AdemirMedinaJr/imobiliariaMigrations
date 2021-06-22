b5c25a2c76f6dedb24540ac692af097b88295dc0
AdemirMedinaJr <54366107+AdemirMedinaJr@users.noreply.github.com>
2021-06-22 01:33:28 -0300

    Delete TipoController.js

--- controllers/TipoController.js
+++
@@ -1,9 +0,0 @@
-const knex = require ("../database/dbConfig");
-
-module.exports = {
-
-    async index(req, res) {
-        const tipos = await knex ("tipos").orderBy("classe");
-        res.status(200).json(tipos);
-    }
-};
