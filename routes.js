d95bcaa39d0b8534d85326f51a23c20977795973
AdemirMedinaJr <ademirmj@hotmail.com>
2021-06-24 03:25:33 -0300

    :hammer: Ajuste no ImovelController

--- routes.js
+++ routes.js
@@ -4,11 +4,11 @@ const cors = require("cors");
 
 routes.use(cors()); // libera todas as rotas para acesso(s) por origens diferentes
 
-const DestacarController = require("./controllers/DestacarController");
-const DestaqueController = require("./controllers/DestaqueController");
-const EstatisticaController = require("./controllers/EstatisticaController");
+//const DestacarController = require("./controllers/DestacarController");
+//const DestaqueController = require("./controllers/DestaqueController");
+//const EstatisticaController = require("./controllers/EstatisticaController");
 const ImovelController = require("./controllers/ImovelController");
-const PesquisarController = require("./controllers/PesquisarController");
+//const PesquisarController = require("./controllers/PesquisarController");
 const PropostaController = require("./controllers/PropostaController");
 const UsuarioController = require("./controllers/UsuarioController");
 const TipoController = require("./controllers/TipoController");
@@ -29,7 +29,7 @@ routes
   //.get("/pesquisar/:filtro", PesquisarController.index)
   //.get("/imoveis/estatistica", EstatisticaController.index)
   //.put("/destacar/:id", DestacarController.update)
-  .post("/proposta/:id", login, PropostaController.store)
+  .post("/proposta/:id", PropostaController.store)
   .delete("/imoveis/:id", ImovelController.destroy);
 
 routes
