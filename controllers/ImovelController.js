ca3e4107dba3fd1e09a86f2124987e9733a14c2b
AdemirMedinaJr <ademirmj@hotmail.com>
2021-06-24 00:08:53 -0300

    :hammer: Ajuste no ImovelController e TipoController

--- controllers/ImovelController.js
+++ controllers/ImovelController.js
@@ -4,15 +4,11 @@ module.exports = {
   // index: listagem
   // store/create: inclusão
   // update: alteração
-  // show: retornar 1 registro
+  // show: obter 1 registro
   // destroy: exclusão
 
   async index(req, res) {
-    //const carros = await knex("carros").orderBy("id", "desc");
-    // const carros = await knex("carros")
-    //    .join("marcas", "carros.marca_id", "=", "marcas.id")
-    //    .orderBy("carros.id", "desc");
-
+  
     const imoveis = await knex
       .select(
         "i.id",
@@ -34,12 +30,87 @@ module.exports = {
     res.status(200).json(imoveis);
   },
 
-  async store(req, res) {
-    // faz a desestruturação do objeto req.body
-    const { cidade, tipo_id, bairro, area, nº_quartos, nº_banheiros, preco, foto } = req.body;
+  async show(req, res) {
+    const id = req.params.id; // ou:  const { id } = req.params
 
-    // validação para os campos
-    if ( !cidade || !tipo_id || !bairro || !area || !nº_quartos || !nº_banheiros || !preco || !foto) {
+    const imovel = await knex
+      .select(
+        "i.id",
+        "i.cidade",
+        "t.classe as tipo",
+        "i.bairro",
+        "i.area",
+        "i.nº_quartos",
+        "i.nº_banheiros",
+        "i.preco",
+        "i.foto",
+        "i.destaque"
+      )
+
+      .from("imoveis as i")
+      .leftJoin("tipos as t", "i.tipo_id", "t.id")
+      .orderBy("i.id", "desc");
+
+    res.status(200).json(imovel[0]);
+  },
+
+  async search(req, res) {
+    const palavra = req.params.palavra;
+
+    const imoveis = await knex
+      .select(
+        "i.id",
+        "i.cidade",
+        "t.classe as tipo",
+        "i.bairro",
+        "i.area",
+        "i.nº_quartos",
+        "i.nº_banheiros",
+        "i.preco",
+        "i.foto",
+        "i.destaque"
+      )
+      .from("imoveis as i")
+      .leftJoin("tipos as t", "i.tipo_id", "t.id")
+      .where("cidade", "like", "%" + palavra + "%")
+      .orWhere("t.classe", "like", "%" + palavra + "%")
+      .orderBy("i.id", "desc");
+    res.status(200).json(imoveis);
+  },
+
+  async store(req, res) {
+    console.log(req.body);
+
+    // desestruturação do objeto request
+    const {
+      cidade,
+      tipo_id,
+      bairro,
+      area,
+      nº_quartos,
+      nº_banheiros,
+      preco,
+      foto,
+    } = req.body;
+
+    if (!cidade) {
+      res.status(400).json({
+        erro: "faltou cidade",
+      });
+      return;
+    }
+
+    // se algum dos atributos não for passado
+    if (
+      !cidade ||
+      !tipo_id ||
+      !bairro ||
+      !area ||
+      !nº_quartos ||
+      !nº_banheiros ||
+      !preco ||
+      !foto
+    )  {
       res.status(400).json({
         erro: "Enviar cidade, imovel_id, bairro, área, nº de quartos, nº de banheiros, preço e foto do imóvel",
       });
@@ -55,11 +126,92 @@ module.exports = {
         nº_quartos,
         nº_banheiros,
         preco,
-        foto
+        foto,
       });
       res.status(201).json({ id: novo[0] });
     } catch (error) {
       res.status(400).json({ erro: error.message });
     }
   },
+
+  async destaque(req, res) {
+    const id = req.params.id; // ou:  const { id } = req.params
+    dados = await knex("imoveis").where({ id });
+    //    console.log(dados[0]);
+
+    if (dados[0].destaque) {
+      try {
+        await knex("imoveis").update({ destaque: 0 }).where({ id });
+        res.status(200).json({ ok: 1 });
+      } catch (error) {
+        res
+          .status(400)
+          .json({ ok: 0, msg: `Erro na alteração: ${error.message}` });
+      }
+    } else {
+      try {
+        await knex("imoveis").update({ destaque: 1 }).where({ id });
+        res.status(200).json({ ok: 1 });
+      } catch (error) {
+        res
+          .status(400)
+          .json({ ok: 0, msg: `Erro na alteração: ${error.message}` });
+      }
+    }
+  },
+
+  async destaques(req, res) {
+    const imoveis = await knex
+      .select(
+        "i.id",
+        "i.cidade",
+        "t.classe as tipo",
+        "i.bairro",
+        "i.area",
+        "i.nº_quartos",
+        "i.nº_banheiros",
+        "i.preco",
+        "i.foto",
+        "i.destaque"
+      )
+      .from("imoveis as i")
+      .leftJoin("tipos as t", "i.tipo_id", "t.id")
+      .where("c.destaque", true)
+      .orderBy("i.id", "desc");
+    res.status(200).json(imoveis);
+  },
+
+  async destroy(req, res) {
+    const id = req.params.id; // ou:  const { id } = req.params
+    try {
+      await knex("imoveis").del().where({ id });
+      res.status(200).json({ ok: 1 });
+    } catch (error) {
+      res
+        .status(400)
+        .json({ ok: 0, msg: `Erro na exclusão: ${error.message}` });
+    }
+  },
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- controllers/TipoController.js
+++ controllers/TipoController.js
@@ -5,5 +5,17 @@ module.exports = {
     async index(req, res) {
         const tipos = await knex ("tipos").orderBy("classe");
         res.status(200).json(tipos);
-    }
+    },
+
+    async tipos_imoveis(req, res) {
+        const tipos = await knex
+          .select("t.classe")
+          .count("i.id as num")
+          .from("tipos as t")
+          .leftOuterJoin("imoveis as i", "t.id", "i.tipo_id")
+          .groupBy("t.classe")
+          .having("num", ">", 0)
+    
+        res.status(200).json(tipos);
+      }
 };
